e4b19362346cf102e71e8fb7c5b2ae712cb4b151
James Proett <jamesproett@JAP.local>
2017-03-08 10:29:28 -0600

    overprinting error solved

--- map.js
+++ map.js
@@ -3,38 +3,63 @@ $(document).ready(function() {
     var currentScore = [];
     var subButton = document.getElementById('submitingGuess')
     var nextButton = document.getElementById('nextGuess')
+    var newGameButton = document.getElementById('newGame')
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var icons = {
+      info: {
+        icon: iconBase + 'info-i_maps.png'
+      }
+    }
     var guessedLocation = null;
-    var haveIGuessed = false
+    var map
+
+
+
+    newGameButton.addEventListener('click', function(event) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: mapViewOfAustin,
+            zoom: 9
+        });
+        createNewPano()
+        clickingStuff()
+    })
 
     function getRndmLaLn(min, max) {
         return Math.random() * (max - min) + min;
     }
 
+    // var randomizedStreetveiw = {
+    //     lat: getRndmLaLn(30.000000, 30.527629),
+    //     lng: getRndmLaLn(-98.074265, -97.417831)
+    // };
+
+    var mapViewOfAustin = {
+        lat: 30.271781,
+        lng: -97.732315
+    };
     var randomizedStreetveiw = {
         lat: getRndmLaLn(30.000000, 30.527629),
         lng: getRndmLaLn(-98.074265, -97.417831)
     };
 
     function randomizeIt() {
-        var randomizedStreetveiw = {
+        randomizedStreetveiw = {
             lat: getRndmLaLn(30.000000, 30.527629),
             lng: getRndmLaLn(-98.074265, -97.417831)
         };
         return randomizedStreetveiw;
     }
 
-    var mapViewOfAustin = {
-        lat: 30.271781,
-        lng: -97.732315
-    };
-    //
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: mapViewOfAustin,
-        zoom: 9
-    });
+    function placeMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    }
 
-    function createNewPano() {
+
+    function createNewPano(toRunLater) {
         var projectionResolved = new Promise(function(res, rej) {
             var panorama = {}
             reRun(panorama, function(result) {
@@ -53,7 +78,7 @@ $(document).ready(function() {
                     // console.log('wtf');
                     reRun(panorama, cb)
                 }
-            }, 500)
+            }, 200)
         }
 
         function fetchNewPanorama() {
@@ -61,6 +86,7 @@ $(document).ready(function() {
                 document.getElementById('pano'), {
                     position: randomizeIt(),
                     addressControl: false,
+                    linksControl: false,
                     pov: {
                         heading: 34,
                         pitch: 10
@@ -69,82 +95,77 @@ $(document).ready(function() {
         }
 
         projectionResolved.then(function(info) {
+            toRunLater()
             console.log('INFO', info.projection);
         })
     }
 
-    createNewPano()
-
-    function placeMarker(location) {
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-    }
-
-    clickingStuff()
-
-
     // clicking on the map on the left will place a marker and show where the street view is located
+
     function clickingStuff (){
         // if (!haveIGuessed) {
             google.maps.event.addListener(map, 'click', function firstGuess(ev) {
                 guessedLocation = ev.latLng
                 placeMarker(ev.latLng);
+
                 return guessedLocation
             });
 
-            subButton.addEventListener('click', function(event) {
-                placeMarker(randomizedStreetveiw);
-                // console.log(ev);
-                // console.log(randomizedStreetveiw);
+            subButton.removeEventListener('click', doStuff)
+            subButton.addEventListener('click', doStuff)
 
-                // making the line on the map
-                var lineOfDifferece = new google.maps.Polyline({
-                    path: [guessedLocation, randomizedStreetveiw],
-                    geodesic: true,
-                    strokeColor: '#FF0000',
-                    strokeOpacity: .7,
-                    strokeWeight: 2
-                });
 
-                // calculating distance of line
-                lineOfDifferece.setMap(map);
-                google.maps.LatLng.prototype.kmTo = function(a) {
-                    var e = Math,
-                        ra = e.PI / 180;
-                    var b = this.lat() * ra,
-                        c = a.lat() * ra,
-                        d = b - c;
-                    var g = this.lng() * ra - a.lng() * ra;
-                    var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d / 2), 2) + e.cos(b) * e.cos(c) * e.pow(e.sin(g / 2), 2)));
-                    return f * 6378.137;
-                }
 
-                google.maps.Polyline.prototype.inKm = function(n) {
-                    var a = this.getPath(n),
-                        len = a.getLength(),
-                        dist = 0;
-                    for (var i = 0; i < len - 1; i++) {
-                        dist += a.getAt(i).kmTo(a.getAt(i + 1));
-                    }
-                    return dist;
-                }
+    }
 
-                var disstanceOfLine = lineOfDifferece.inKm();
-                currentScore.push(disstanceOfLine)
-                console.log(disstanceOfLine);
-                console.log(currentScore);
-                // document.getElementById('scoreBox').innerHTML = Math.floor(disstanceOfLine) + ' Kilometers Away From Location!'
-                window.alert(Math.floor(disstanceOfLine) + ' Kilometers Away From Location!');
-                var totalScore = null;
-                for (var i = 0; i < currentScore.length; i++) {
-                    totalScore += currentScore[i]
-                }
-                console.log(totalScore);
-                // $('#scoreBox')append.val(totalScore)
-            })
+    function doStuff(event) {
+        placeMarker(randomizedStreetveiw);
 
+
+        // making the line on the map
+        var lineOfDifferece = new google.maps.Polyline({
+            path: [guessedLocation, randomizedStreetveiw],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: .7,
+            strokeWeight: 2
+        });
+
+        // calculating distance of line
+        lineOfDifferece.setMap(map);
+        google.maps.LatLng.prototype.kmTo = function(a) {
+            var e = Math,
+            ra = e.PI / 180;
+            var b = this.lat() * ra,
+            c = a.lat() * ra,
+            d = b - c;
+            var g = this.lng() * ra - a.lng() * ra;
+            var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d / 2), 2) + e.cos(b) * e.cos(c) * e.pow(e.sin(g / 2), 2)));
+            return f * 6378.137;
+        }
+
+        google.maps.Polyline.prototype.inKm = function(n) {
+            var a = this.getPath(n),
+            len = a.getLength(),
+            dist = 0;
+            for (var i = 0; i < len - 1; i++) {
+                dist += a.getAt(i).kmTo(a.getAt(i + 1));
+            }
+            return dist;
+        }
+
+        var disstanceOfLine = lineOfDifferece.inKm();
+        currentScore.push(disstanceOfLine)
+
+        // document.getElementById('scoreBox').innerHTML = Math.floor(disstanceOfLine) + ' Kilometers Away From Location!'
+        window.alert(Math.round(disstanceOfLine) + ' Kilometers Away From Location!');
+        var totalScore = null
+        for (var i = 0; i < currentScore.length; i++) {
+            totalScore += currentScore[i]
+        }
+        console.log(totalScore);
+        console.log(currentScore);
+        // $('#scoreBox')append.val(totalScore)
     }
 
     nextButton.addEventListener('click', function(event) {
@@ -152,15 +173,16 @@ $(document).ready(function() {
             center: mapViewOfAustin,
             zoom: 9
         });
-        createNewPano()
-        clickingStuff()
-
+        createNewPano(clickingStuff)
+        // subButton.removeEventListener('click', doStuff)
 
     });
 
 
 
 })
+
+//is the problem partily  becouse of the two randomize fucntions not reassigning each time
 
 
 // api_key=mapzen-odes-cMfGEs
